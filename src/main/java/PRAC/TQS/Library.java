10e39dc3e63e5e1c089ad8935fd9b7f755b2320c
1490917 <72441147+1490917@users.noreply.github.com>
2020-10-14 16:14:30 +0200

    test TRUE BIEN

--- src/main/java/PRAC/TQS/Library.java
+++ src/main/java/PRAC/TQS/Library.java
@@ -7,11 +7,4 @@ public class Library {
     public boolean someLibraryMethod() {
         return true;
     }
-<<<<<<< HEAD
 }
-=======
-}
-/*
-* dsada sd
-*/
->>>>>>> 7192b1fcfceeda76ccc9148631435dd4ac4a7c3b
